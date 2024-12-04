aa1b8baa3c7b1d8c41e9536a7ca566f148d72fbf
Mae Barnhoorn <mae@connectstuff.nl>
2024-12-04 14:24:59 +0100

    initial commit

---
+++ app/code/Vendor/OrderNotifier/Observer/OrderCreated.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Vendor\OrderNotifier\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\HTTP\Client\Curl;
+use Psr\Log\LoggerInterface;
+
+class OrderCreated implements ObserverInterface
+{
+    protected $curl;
+    protected $logger;
+
+    public function __construct(Curl $curl, LoggerInterface $logger)
+    {
+        $this->curl = $curl;
+        $this->logger = $logger;
+    }
+
+    public function execute(Observer $observer)
+    {
+        try {
+            // Get the order object
+            $order = $observer->getEvent()->getOrder();
+
+            // Prepare data for HTTP request
+            $data = [
+                'key' => 'ZKzU^_AP2n51?a)qFQ*-8h{debk]?}@Ou_U%<//ScuZBpjO0<BYI4M.&fT3)k',
+                'order_id' => $order->getIncrementId(),
+                'status' => $order->getStatus(),
+                'status_label' => $order->getStatusLabel(),
+                'created_at' => $order->getCreatedAt(),
+                'subtotal' => $order->getSubtotal(),
+                'base_grand_total' => $order->getBaseGrandTotal(),
+                'grand_total' => $order->getGrandTotal(),
+                'customer_id' => $order->getCustomerId(),
+                'base_currency_code' => $order->getBaseCurrencyCode(),
+                'global_currency_code' => $order->getGlobalCurrencyCode(),
+                'order_currency_code' => $order->getOrderCurrencyCode(),
+                'payment_method' => $result->getPayment()->getMethod(),
+            ];
+
+            // Make HTTP POST request
+            $url = 'https://magento-integ-7.hoststuff.nl/api/order'; 
+            $this->curl->post($url, json_encode($data));
+
+            // Log the response
+            $response = $this->curl->getBody();
+        } catch (\Exception $e) {
+            $this->logger->error('Order Notifier Error: ' . $e->getMessage());
+        }
+    }
+}
